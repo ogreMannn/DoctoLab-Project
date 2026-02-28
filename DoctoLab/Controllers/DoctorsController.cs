f952f36ba993464409b9d497fc0b962cbe117661
Sky <elcanneceffli@gmail.com>
2026-02-28 15:46:32 +0400

    Added Get func for DoctorController

--- DoctoLab/Controllers/DoctorsController.cs
+++ DoctoLab/Controllers/DoctorsController.cs
@@ -1,4 +1,6 @@
 ﻿using DoctoLab.Contexts;
+using DoctoLab.DTOs;
+using DoctoLab.GTOs;
 using DoctoLab.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +15,37 @@ namespace DoctoLab.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        
+        public DoctorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DoctorGetDto>>> GetAll()
+        {
+            var doctors = await _context.Doctors
+                .Select(x => new DoctorGetDto
+                {
+
+                    Id = x.Id,
+                    Name = x.Name,
+                    Surname = x.Surname,
+                    Age = x.Age,
+                    Description = x.Description,
+                    FilePath = x.FilePath,
+                    FieldId = x.FieldId,
+                    FieldName = x.field.Name,
+                    HospitalId = x.HospitalId,
+                    HospitalName = x.hospital.Name
+
+
+                }).ToListAsync();
+
+            return Ok(doctors);
+        }
+
+       
+
+
     }
 }
